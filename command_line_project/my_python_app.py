507f91a97d57b1a45383c0189aff336d399ffede
Replit user <>
2023-07-28 12:23:56 +0000

    Number Guessing Name

--- command_line_project/my_python_app.py
+++ command_line_project/my_python_app.py
@@ -1,27 +1,48 @@
 import random
 
-def number_guessing_game():
+def display_question_mark():
+    question_mark = """ 
+        -
+        _jgN########Ngg_
+      _N##N@@""  ""9NN##Np_
+     d###P            N####p
+     "^^"              T####
+                       d###P
+                    _g###@F
+                 _gN##@P
+               gN###F"
+              d###F
+             0###F
+             0###F
+             0###F
+             "NN@'
+    
+              ___
+             q###r
+              ""
+    """
+    print(question_mark)
+
+def guess_number_game():
+    display_question_mark()
+    print("Hello and welcome to Number guessing game!")
     secret_number = random.randint(1, 100)
 
-    start = 1
-    end = 100
-    random_number = random.randint(start, end)
-    print(random_number)
-
-    start = 1
-    end = 100
-
     guess = int(input("Guess a number between 1 and 100: "))
+    while guess != secret_number:
+        if guess < secret_number:
+            print("Too low. Try again.")
+            guess = int(input("Guess a number between 1 and 100: "))
+        else:
+            print("Too high. Try again.")
+            guess = int(input("Guess a number between 1 and 100: "))
+    try:
+        if guess == secret_number:
+            print("Congratulations! You guessed the correct number.")
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
 
-    if guess == secret_number:
-        print("Congratulations! You guessed the correct number.")
-    elif guess < secret_number:
-        print("Too low. Try again.")
-    else:
-        print("Too high. Try again.")
+if __name__ == "__main__":
+    guess_number_game()
 
-    categories = ("even", "odd")
-    for category in categories:
-        print(category)
 
-number_guessing_game()
